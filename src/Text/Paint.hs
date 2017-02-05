30b8741bbed763fee3c997849a84a1b3363a7745
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-02-05 23:36:45 +0000

    added support for 16 colors for both layers

--- src/Text/Paint.hs
+++ src/Text/Paint.hs
@@ -21,15 +21,23 @@ import qualified Data.Text as T
 
 -- | Foreground and background color.
 data Color
-  = Black   -- ^ black
-  | Red     -- ^ red
-  | Green   -- ^ green
-  | Yellow  -- ^ yellow
-  | Blue    -- ^ blue
-  | Magenta -- ^ magenta
-  | Cyan    -- ^ cyan
-  | White   -- ^ white
-  | Default -- ^ default setting
+  = Black        -- ^ black
+  | Red          -- ^ red
+  | Green        -- ^ green
+  | Yellow       -- ^ yellow
+  | Blue         -- ^ blue
+  | Magenta      -- ^ magenta
+  | Cyan         -- ^ cyan
+  | LightGray    -- ^ light gray
+  | DarkGray     -- ^ dark gray
+  | LightRed     -- ^ light red
+  | LightGreen   -- ^ light green
+  | LightYellow  -- ^ light yellow
+  | LightBlue    -- ^ light blue
+  | LightMagenta -- ^ light magenta
+  | LightCyan    -- ^ light cyan
+  | White        -- ^ white
+  | Default      -- ^ default setting
   deriving (Eq, Ord, Enum, Show)
 
 -- | Various flags applicable to the text.
@@ -53,14 +61,44 @@ flagCode Blink     = 5
 -- | Convert a foreground color into an ANSI code.
 fgCode :: Color -- ^ color
        -> Int   -- ^ ANSI code
-fgCode Default = 39
-fgCode color   = 30 + fromEnum color
+fgCode Black        = 30
+fgCode Red          = 31
+fgCode Green        = 32
+fgCode Yellow       = 33
+fgCode Blue         = 34
+fgCode Magenta      = 35
+fgCode Cyan         = 36
+fgCode LightGray    = 37
+fgCode Default      = 39
+fgCode DarkGray     = 90
+fgCode LightRed     = 91
+fgCode LightGreen   = 92
+fgCode LightYellow  = 93
+fgCode LightBlue    = 94
+fgCode LightMagenta = 95
+fgCode LightCyan    = 96
+fgCode White        = 97
 
 -- | Convert a background color into an ANSI code.
 bgCode :: Color -- ^ color
        -> Int   -- ^ ANSI code
-bgCode Default = 49
-bgCode color   = 40 + fromEnum color
+bgCode Black        = 40
+bgCode Red          = 41
+bgCode Green        = 42
+bgCode Yellow       = 43
+bgCode Blue         = 44
+bgCode Magenta      = 45
+bgCode Cyan         = 46
+bgCode LightGray    = 47
+bgCode Default      = 49
+bgCode DarkGray     = 100
+bgCode LightRed     = 101
+bgCode LightGreen   = 102
+bgCode LightYellow  = 103
+bgCode LightBlue    = 104
+bgCode LightMagenta = 105
+bgCode LightCyan    = 106
+bgCode White        = 107
 
 -- | Convert a coloring scheme into an ANSI escape sequence.
 translate :: Paint  -- ^ coloring scheme
