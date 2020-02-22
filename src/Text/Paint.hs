59325586d4a09c5366a11768ba488ea8d4e52123
Daniel Lovasko <daniel.lovasko@gmail.com>
2020-02-22 14:33:14 +0000

    alternate funciton and data type formatting

--- src/Text/Paint.hs
+++ src/Text/Paint.hs
@@ -49,18 +49,21 @@ data Flag
 
 -- | Coloring scheme containing foreground and background layers, and a
 -- setting for text styling.
-data Paint = Paint Color Color [Flag]
+data Paint
+  = Paint Color Color [Flag]
 
 -- | Convert a flag into an ANSI code.
-flagCode :: Flag -- ^ flag
-         -> Int  -- ^ ANSI code
+flagCode
+  :: Flag -- ^ flag
+  -> Int  -- ^ ANSI code
 flagCode Bold      = 1
 flagCode Underline = 4
 flagCode Blink     = 5
 
 -- | Convert a foreground color into an ANSI code.
-fgCode :: Color -- ^ color
-       -> Int   -- ^ ANSI code
+fgCode
+  :: Color -- ^ color
+  -> Int   -- ^ ANSI code
 fgCode Black   = 30
 fgCode Maroon  = 31
 fgCode Green   = 32
@@ -80,8 +83,9 @@ fgCode Aqua    = 96
 fgCode White   = 97
 
 -- | Convert a background color into an ANSI code.
-bgCode :: Color -- ^ color
-       -> Int   -- ^ ANSI code
+bgCode
+  :: Color -- ^ color
+  -> Int   -- ^ ANSI code
 bgCode Black   = 40
 bgCode Maroon  = 41
 bgCode Green   = 42
@@ -101,15 +105,17 @@ bgCode Aqua    = 106
 bgCode White   = 107
 
 -- | Convert a coloring scheme into an ANSI escape sequence.
-translate :: Paint  -- ^ coloring scheme
-          -> T.Text -- ^ escape sequence
+translate
+  :: Paint  -- ^ coloring scheme
+  -> T.Text -- ^ escape sequence
 translate (Paint fg bg flags) = T.concat [T.pack "\x1b[", codes, T.pack "m"]
   where
     codes  = T.intercalate (T.singleton ';') $ map (T.pack . show) codes'
     codes' = map flagCode flags ++ [fgCode fg] ++ [bgCode bg]
 
 -- | Apply a color scheme to a text instance.
-paint :: Paint  -- ^ coloring scheme
-      -> T.Text -- ^ plain text
-      -> T.Text -- ^ colorized text
+paint
+  :: Paint  -- ^ coloring scheme
+  -> T.Text -- ^ plain text
+  -> T.Text -- ^ colorized text
 paint pnt text = T.concat [translate pnt, text, T.pack "\x1b[0m"]
